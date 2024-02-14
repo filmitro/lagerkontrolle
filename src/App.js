44fd1769d1ca6af1150ef8011a3167dccbfd7ca0
filmitro <filmitro@gmail.com>
2024-02-14 20:59:31 +0100

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import { useState } from "react";
 import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9XkRjUWytUePuGxx9DDo0t0Auyr0nCgI",
+  authDomain: "lagerkontrolle-bfe9c.firebaseapp.com",
+  projectId: "lagerkontrolle-bfe9c",
+  storageBucket: "lagerkontrolle-bfe9c.appspot.com",
+  messagingSenderId: "528589231187",
+  appId: "1:528589231187:web:33be718a559c5b4a4e22a6",
+  measurementId: "G-KG1F6Y3J8K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   const [todos, setTodos] = useState([]);
 
